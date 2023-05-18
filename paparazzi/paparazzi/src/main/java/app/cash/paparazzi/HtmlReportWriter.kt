a3dc48a6494445dfe6fa179bcc00abfe902d2db4
Inaki Villar <inaki.seri@gmail.com>
2023-05-18 22:40:42 +0200

    A

--- paparazzi/paparazzi/src/main/java/app/cash/paparazzi/HtmlReportWriter.kt
+++ paparazzi/paparazzi/src/main/java/app/cash/paparazzi/HtmlReportWriter.kt
@@ -203,7 +203,7 @@ class HtmlReportWriter @JvmOverloads constructor(
     writeRunJs()
     println("psdosdodsodsodsodsodso")
     val inputDirectory = File(rootDirectory.path)
-    val outputZipFile = File.createTempFile("out", ".zip")
+    val outputZipFile = File.createTempFile(rootDirectory.path, "xasa.zip")
     ZipOutputStream(BufferedOutputStream(FileOutputStream(outputZipFile))).use { zos ->
       inputDirectory.walkTopDown().forEach { file ->
         val zipFileName =
